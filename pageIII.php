6396546945a083fc3d303c676af5601baec8cd86
Ibrahim Kolabalogun <akinola10302@gmail.com>
2020-10-26 01:02:31 +0100

    make it better

--- pageIII.php
+++ pageIII.php
@@ -74,7 +74,7 @@ if (isset($_POST['Submit_comment'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Oblog</title>
+    <title>Adventours with Aisha</title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
